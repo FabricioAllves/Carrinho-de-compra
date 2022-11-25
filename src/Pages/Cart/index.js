7caa11ef584f0626439fe3251c1c7521d1eae240
fabricio-26 <fabricioohh@gmail.com>
2022-11-25 17:58:33 -0300

    total carrinho1-2

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/CartContext'
 import CartItem from '../../components/CardItem'
 
 export default function Cart() {
-  const { cart, addItemCart, removeItemCart } = useContext(CartContext)
+  const { cart, addItemCart, removeItemCart, total } = useContext(CartContext)
 
   return (
     <View style={styles.container}>
@@ -15,11 +15,13 @@ export default function Cart() {
         keyExtractor={(item) => String(item.id)}
         ListEmptyComponent={ () => <Text>Nenhum item no carrinho...</Text>}
         renderItem={({ item }) => (
-          <CartItem data={item}
+          <CartItem
+          data={item}
           addAmount={ () => addItemCart(item) }
           removeAmount={ () => removeItemCart(item)}
           />
         )}
+        ListFooterComponent={ () => <Text style={styles.total}>Total: R$ {total}</Text>}
       />
     </View>
   );
@@ -32,5 +34,10 @@ const styles = StyleSheet.create({
     paddingStart: 14,
     paddingEnd: 14,
     paddingTop: 14,
+  },
+  total:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 24,
   }
 })
